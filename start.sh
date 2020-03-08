16e98986c1e8243326bf646f19879bbced7d35c2
Nick <webjema@gmail.com>
2020-03-08 21:31:43 +0100

    Tasks quantity as EBV value

--- start.sh
+++ start.sh
@@ -4,6 +4,6 @@ dockerd &
 echo "Downloading JAR"
 wget --no-check-certificate $JAR_FILE_URL
 echo "Starting Docker Compose"
-docker-compose up -d --scale chrome=3
+docker-compose up -d --scale chrome=$PARALLEL_TASKS_QNT
 echo "Starting CrawlerService"
 java -jar CrawlerService-1.0-SNAPSHOT-jar-with-dependencies.jar CrawlerTasks
